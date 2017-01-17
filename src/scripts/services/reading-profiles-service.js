c698eaaa55cbb837229213351bdf6de4eefae430
Marina Kraeva <marina.kraeva@asteasolutions.com>
2017-01-17 17:12:26 +0200

    Document parameters for ReadingProfilesService#saveProfile

--- src/scripts/services/reading-profiles-service.js
+++ src/scripts/services/reading-profiles-service.js
@@ -11,7 +11,23 @@ angular.module('intellogoSDK')
   .service('ReadingProfilesService',
       function ($http, RatingService, ServiceUtils, API_LOCATION) {
 
-        function saveProfile (profile, callback) {
+          /**
+           * Save changes to the given profile, or adds it to the Intellogo system if a new one.
+           * Changes to the profile's contents list require a Content object that has at least a
+           * _id property.
+           * @param {Object} profile The profile to save
+           * @param {String} [profile._id] The id of the profile to update, if it exists.
+           * @param {String} profile.clientReference The client reference value for the profile.
+           * @param {Array<Content>} profile.assignedContents An array of new contents to assign
+           * to a profile. Content already included in the profile does not need to be specified
+           * and will not be removed (unless it is included in the unassignedContents parameter).
+           * @param {Array<Content>} profile.unassignedContents An array of content entities to
+           * remove from the reading profile definition.
+           * @param {Function} callback Will be called after the profile is saved, or if an error
+           * occurs. If successful, the callback will be called with the profile data, with its
+           * _id field set.
+           */
+          function saveProfile (profile, callback) {
             var contentIdsToAssign =
                     _.pluck(profile.assignedContents, '_id'),
                 contentIdsToUnassign =
